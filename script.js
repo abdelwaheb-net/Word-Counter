39138aac8e4b788209293b950d400ff91b41eb0f
Abdelwaheb Ammar <abdelwaheb_ammar@hotmail.fr>
2025-05-12 01:06:52 +0100

    update on word counter

--- script.js
+++ script.js
@@ -6,6 +6,25 @@ $(document).ready(function () {
     let words = text.trim().split(/\s+/);
     let wordCount = text === "" ? 0 : words.length;
     $("#wordCount").text(wordCount);
+    let characterwithspace=text.trim().length;
+ let characterwithoutspace=text.replace(/\s+/g, '').length;
+
+ $("#charCount").text(characterwithspace);
+ $("#charCountNoSpace").text(characterwithoutspace);
+
+
+
+
+  });
+
+  $("#resetBtn").on("click", function () {
+    console.log("hello world");
+    $("#textInput").val("");
+    $("#wordCount").text("0");
+    $("#charCount").text("0");
+    $("#charCountNoSpace").text("0");
+
+    
   });
 
 });
